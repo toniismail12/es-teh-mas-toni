e3f31bed5637fb0895bbb1e3f92c7c78c76601a5
toniismailz <tonismail20@gmail.com>
2024-11-29 09:46:53 +0700

    creata mfa page - verify otp

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -23,6 +23,53 @@ async function GetAuthme() {
 
 }
 
+async function CheckAuth() {
+
+    try {
+
+        const cookieString = document?.cookie
+        var cleanToken = "-";
+        if(cookieString) {
+            cleanToken = cookieString.split(';').find(cookie => cookie.trim().startsWith('access_token=')).split('=')[1];
+        }
+
+        const res = await svc_sso.get("/protect/check-auth", {
+            headers: {
+                Authorization: `Bearer ${cleanToken}`,
+            },
+        });
+        // console.log('Response:', res);
+        return res
+
+    } catch (error) {
+        console.error('Error:', error);
+        window.location.href = process.env.NEXT_PUBLIC_API_SVC_SSO+"/api/login?redirect_to="+process.env.NEXT_PUBLIC_URL
+    }
+
+}
+
+async function VerifyOTP(code, redirect) {
+
+    const data = {
+        kode: code,
+        redirect_to: redirect
+    }
+
+    try {
+        await svc_sso.post("/protect/verify-otp", data);
+
+        const decoded = atob(redirect);
+        // console.log('Response:', res);
+        window.location.href = decoded
+
+    } catch (error) {
+        console.error('Error:', error);
+        // alert("error update data")
+        return "error"
+    }
+
+}
+
 async function Logout() {
 
     const cookieString = document?.cookie
@@ -38,7 +85,7 @@ async function Logout() {
     try {
         const res = await svc_sso.get("/api/logout", config);
         // console.log('Response:', res);
-        return res?.data
+        return res
 
     } catch (error) {
         console.error('Error:', error);
@@ -48,4 +95,4 @@ async function Logout() {
 }
 
 export default GetAuthme
-export { GetAuthme, Logout }
+export { GetAuthme, Logout, CheckAuth, VerifyOTP }
--- src/pages/success.js
+++ src/pages/success.js
@@ -1,10 +1,95 @@
 import React from 'react'
+import { VerifyOTP, CheckAuth } from '@/controllers';
+import { useRouter } from 'next/router';
+import { useEffect, useCallback } from 'react';
 
 export default function SuccesPage() {
-    
+
+  const { query } = useRouter()
+
+  const fetchData = useCallback(async () => {
+
+    await CheckAuth()
+
+  }, []);
+
+  useEffect(() => {
+
+    fetchData()
+
+  }, [fetchData])
+
+  async function PostKode() {
+    // Collect values from the input fields
+    const kode = Array.from({ length: 6 }, (_, i) => {
+      const inputElement = document.getElementById(`text${i + 1}`);
+      return inputElement?.value || ""; // Get the value or default to an empty string
+    }).join(""); // Join the array values into a single string
+
+    const redirect = query.redirect_uri; // Get redirect URL from environment variable
+
+    if (kode.length === 6) { // Ensure all inputs are filled
+      await VerifyOTP(kode, redirect);
+    } else {
+      alert("Please fill all input fields.");
+    }
+  }
+
   return (
-    <div>
-      Redirect ... Please Wait ...
+    <div className="position-relative overflow-hidden radial-gradient min-vh-100 w-100 d-flex align-items-center justify-content-center">
+      <div className="d-flex align-items-center justify-content-center w-100">
+        <div className="row justify-content-center w-100">
+          <div className="col-md-8 col-lg-4">
+            <div className="card mb-0">
+              <div className="card-body pt-5">
+
+                <div className="mb-5 text-center">
+
+                  <h5 className="fw-bolder mb-3">Multifactor Authentication</h5>
+
+                  <p>
+                    We sent a verification code to your mobile. Enter the code from
+                    the mobile in the field below.
+                  </p>
+
+                </div>
+                  <div className="mb-3">
+                    <label
+                      className="form-label fw-semibold"
+                    >
+                      Type your 6 digits security code
+                    </label>
+                    <div className="d-flex align-items-center gap-2 gap-sm-3">
+                      <input id='text1' maxLength={1} type="text" className="form-control" placeholder="" />
+                      <input id='text2' maxLength={1} type="text" className="form-control" placeholder="" />
+                      <input id='text3' maxLength={1} type="text" className="form-control" placeholder="" />
+                      <input id='text4' maxLength={1} type="text" className="form-control" placeholder="" />
+                      <input id='text5' maxLength={1} type="text" className="form-control" placeholder="" />
+                      <input id='text6' maxLength={1} type="text" className="form-control" placeholder="" />
+                    </div>
+                  </div>
+                  <button
+                    onClick={PostKode}
+                    className="btn btn-primary w-100 py-8 mb-4 d-flex align-item-center justify-content-center"
+                  >
+                    <i className="ti ti-check fs-6 me-2"></i>
+                    Verify
+                  </button>
+                  <div className="d-flex align-items-center">
+                    <p className="fs-4 mb-0 text-dark">Didn`t get the code?</p>
+                    <a
+                      className="text-primary fw-medium ms-2"
+                      href="javascript:void(0)"
+                    >
+                      Resend
+                    </a>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
   )
 }
